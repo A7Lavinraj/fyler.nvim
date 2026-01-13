ea9eb3fe1bfe74d0235db781dca2414d2899166a
A7Lavinraj <lavinrajmohan@gmail.com>
2026-01-13 17:02:00 +0530

    test: Add mock win functions for CI compatibility

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -19,4 +19,23 @@ for o, v in pairs({
   writebackup = false,
 }) do vim.opt[o] = v end
 
+local mock_title = function(x)
+  if type(x) ~= "string" then return x end
+  return x:gsub("^.*/.temp/data", "MOCK_ROOT/data")
+end
+
+_G.nvim_open_win_orig = vim.api.nvim_open_win
+
+vim.api.nvim_open_win = function(buf_id, enter, config)
+  config.title = mock_title(config.title)
+  return nvim_open_win_orig(buf_id, enter, config)
+end
+
+_G.nvim_win_set_config_orig = vim.api.nvim_win_set_config
+
+vim.api.nvim_win_set_config = function(win_id, config)
+  config.title = mock_title(config.title)
+  return nvim_win_set_config_orig(win_id, config)
+end
+
 _G.FYLER_TEMP_DIR = vim.fs.joinpath(vim.uv.cwd(), ".temp")
